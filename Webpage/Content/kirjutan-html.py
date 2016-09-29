6bd17de665035a5134fac9e0b8affbf846ab5945
peeter-t2 <peeter.tinits@gmail.com>
2016-09-29 10:30:45 +0300

    redesign

--- Webpage/Content/kirjutan-html.py
+++ Webpage/Content/kirjutan-html.py
@@ -2,7 +2,6 @@
 # -*- coding: utf-8 -*-
 # kirjutan-html.py
 
-f = open('Teremaalim.html','w')
 
 with open('head_et.txt','r') as myfile:
     head_et=myfile.read().replace('\n', '')
@@ -12,16 +11,30 @@ with open('home_et.txt','r') as myfile:
     home_et=myfile.read().replace('\n', '')
 with open('rightcol_et.txt','r') as myfile:
     rightcol_et=myfile.read().replace('\n', '')
+#with open('whatis_et.txt','r') as myfile:
+#    rightcol_et=myfile.read().replace('\n', '')
 
-sonumalgus = """<html>
-<head></head>
-"""
-sonumlopp = """<body><p>Tere maalim!</p></body>
-</html>"""
 
-sonum = head_et + leftcol_et + home_et + rightcol_et
+with open('head_en.txt','r') as myfile:
+    head_en=myfile.read().replace('\n', '')
+with open('leftcol_en.txt','r') as myfile:
+    leftcol_en=myfile.read().replace('\n', '')
+with open('home_en.txt','r') as myfile:
+    home_en=myfile.read().replace('\n', '')
+with open('rightcol_en.txt','r') as myfile:
+    rightcol_en=myfile.read().replace('\n', '')
+#with open('whatis_et.txt','r') as myfile:
+#    rightcol_et=myfile.read().replace('\n', '')
 
-f.write(sonum)
+
+f = open('index_et.html','w')
+sisu = head_et + leftcol_et + home_et + rightcol_et
+f.write(sisu)
+f.close()
+
+f = open('index_en.html','w')
+sisu = head_en + leftcol_en + home_en + rightcol_en
+f.write(sisu)
 f.close()
 
 
